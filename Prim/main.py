bb0826681f3febb81db82da6f40a9c03ac4759cf
QueraltSM <queraltsosa@hotmail.com>
2019-02-20 11:36:12 +0000

    Update main.py

--- Prim/main.py
+++ Prim/main.py
@@ -7,6 +7,7 @@ def prim(graph, root):
     visited = [root]
     path = []
     next = None
+    cost = 0
 
     while nodes:
         distance = float('inf')
@@ -19,19 +20,20 @@ def prim(graph, root):
                     pre = s
                     next = d
         path.append((pre, next))
+        cost += distance
         visited.append(next)
         nodes.remove(next)
 
-    return path
+    print "Path = ",path
+    print "Minimum cost = ",cost
 
 
 if __name__ == '__main__':
-    graph_dict = {  "1":{"1": 0, "2": 2, "3": 1, "4": 4, "5": 3},
-                    "2":{"1": 1, "2": 0, "3": 4, "4": 2, "5": 2},
-                    "3":{"1": 2, "2": 1, "3": 0, "4": 1, "5": 4},
-                    "4":{"1": 3, "2": 5, "3": 2, "4": 0, "5": 1},
-                    "5":{"1": 3, "2": 5, "3": 2, "4": 4, "5": 0},
+    graph_dict = {  "1":{"1": 0, "2": 8, "3": 4, "4": 2, "5": 9},
+                    "2":{"1": 8, "2": 0, "3": 6, "4": 6, "5": 5},
+                    "3":{"1": 4, "2": 6, "3": 0, "4": 4, "5": 1},
+                    "4":{"1": 2, "2": 6, "3": 4, "4": 0, "5": 3},
+                    "5":{"1": 9, "2": 5, "3": 1, "4": 3, "5": 0},
     }
 
-    path = prim(graph_dict, '1')
-    print path
+    prim(graph_dict, '1')
